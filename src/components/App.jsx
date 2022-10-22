1aa53d7a9145ae3c6e1699c95c534481caafd7d2
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-10-22 15:41:20 +0300

    add save to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import { Filter } from './Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -56,6 +51,25 @@ class App extends Component {
       };
     });
   };
+
+  componentDidMount() {
+    try {
+      const LSContacts = localStorage.getItem('contacts');
+      if (LSContacts !== null) {
+        this.setState(() => {
+          return { contacts: JSON.parse(LSContacts) };
+        });
+      }
+    } catch (error) {
+      console.error('Get state error: ', error.message);
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { filter, contacts } = this.state;
